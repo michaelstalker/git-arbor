e83137c9df82bc791af2cf94544899127399606f
Michael Stalker <mtstalker@gmail.com>
2013-12-20 14:08:54 -0500

    Allow user to specify age of branches

--- lib/git_arbor.rb
+++ lib/git_arbor.rb
@@ -1,13 +1,21 @@
-require 'date'
+require 'trollop'
 require_relative './git_arbor/old_branches'
 
 module GitArbor
   class Runner
-
-    def run(arguments)
-      branches = GitArbor::OldBranches.new
+    def run
+      options = fetch_arguments
+      branches = GitArbor::OldBranches.new(days_old: options[:age])
       branches.identify
       branches.print
+    end
+
+    private
+
+    def fetch_arguments
+      Trollop.options do
+        opt :age, 'Age in days', type: :integer
+      end
     end
   end
 end
--- lib/git_arbor/old_branches.rb
+++ lib/git_arbor/old_branches.rb
@@ -1,29 +1,31 @@
+require 'date'
+
 module GitArbor
   class OldBranches
-    AGE_IN_DAYS = 180
-    DATE_THRESHOLD = Date.today - AGE_IN_DAYS
+    attr_reader :days_old, :date_threshold
 
-    def initialize
+    def initialize(options = {})
+      @days_old = options[:days_old] || 180
       @pruning_candidates = []
     end
 
     def identify
       branch_list = `git branch`
-      branches = branch_array branch_list
+      branches = branch_array(branch_list)
 
       branches.each do |branch|
-        @pruning_candidates << branch if branch_date_time(branch) < DATE_THRESHOLD
+        @pruning_candidates << branch if branch_date_time(branch) < date_threshold
       end
     end
 
     def print
-      puts "Old branches (older than #{AGE_IN_DAYS} days old):"
+      puts "Old branches (older than #{days_old} days old):"
 
       @pruning_candidates.each do |branch|
         print_date(branch)
       end
     end
-    
+
     private
 
     def branch_array(branch_list)
@@ -40,13 +42,17 @@ module GitArbor
 
     def branch_date_time(branch)
       date_time_string = `git show #{branch} --format="%ci" | head -n 1`
-      date_time = ::DateTime.parse date_time_string
+      date_time = DateTime.parse(date_time_string)
     end
 
     def print_date(branch)
-      date_time = branch_date_time branch
+      date_time = branch_date_time(branch)
       date = date_time.to_date
       puts branch + " - #{date}"
+    end
+
+    def date_threshold
+      Date.today - days_old
     end
   end
 end
